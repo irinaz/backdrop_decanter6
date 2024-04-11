ec96fec927c51a3c4713fe1a8e108afbff464a48
Boris Ayupov <boris.ayupov@gmail.com>
2024-04-11 11:10:21 -0400

    Update search header.tpl.php

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -19,6 +19,27 @@
  * - $menu: The menu for the header (if any), as an HTML string.
  */
 ?>
+<?php
+/**
+ * @file
+ * Display generic site information such as logo, site name, etc.
+ *
+ * Available variables:
+ *
+ * - $base_path: The base URL path of the Backdrop installation. At the very
+ *   least, this will always default to /.
+ * - $directory: The directory the template is located in, e.g. modules/system
+ *   or themes/bartik.
+ * - $is_front: TRUE if the current page is the front page.
+ * - $logged_in: TRUE if the user is registered and signed in.
+ * - $logo: The path to the logo image, as defined in theme configuration.
+ * - $front_page: The URL of the front page. Use this instead of $base_path, when
+ *   linking to the front page. This includes the language domain or prefix.
+ * - $site_name: The name of the site, empty when display has been disabled.
+ * - $site_slogan: The site slogan, empty when display has been disabled.
+ * - $menu: The menu for the header (if any), as an HTML string.
+ */
+?>
 <a href="#main-content" class="su-skiplinks ">Skip to main content</a>
 <div class="su-brand-bar ">
   <div class="su-brand-bar__container">
@@ -28,9 +49,16 @@
     </a>
   </div>
 </div>
-<section class="container">
-  <section class="flex-container">
-    <?php if ($site_name || $logo || $site_slogan): ?>
+
+<nav class="header-menu"><ul class="header-menu-list"><?php if ($logged_in): ?><li class="first leaf menu-mlid-2"><a href="/user">My account</a></li><li class="last leaf menu-mlid-14"><a href="/user/logout">Log out</a></li><?php endif;
+?><?php if (!$logged_in): ?><li class="last leaf menu-mlid-14"><a href="/user/login">Log in</a></li><?php endif;
+?></ul></nav>
+
+<section class="su-masthead [ modifier_class ]">
+
+		<section>
+
+    <?php if ($site_name || $logo): ?>
       <a class="su-lockup" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
         <?php if ($logo): ?>
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
@@ -40,13 +68,40 @@
         <?php endif; ?>
       </a>
     <?php endif; ?>
-    <?php if ($site_slogan): ?>
-      <div class="header-site-slogan"><?php print $site_slogan; ?></div>
-    <?php endif; ?>
+				
+    <div class="su-site-search" role="search">
+      <form action="/search" method="get" accept-charset="UTF-8">
+        <div class="form-type-search form-item-keys">
+          <label for="edit-keys" class="su-site-search__sr-label">Enter your keywords </label>
+          <input class="form-search form-control form-search su-site-search__input" type="search" id="edit-keys" name="keys" value="" size="40" maxlength="255">
+        </div>
+        <div class="form-actions" id="edit-actions"><input class="btn-primary btn form-submit su-site-search__submit su-sr-only-text" type="submit" id="edit-submit" name="op" value=""></div>
+      </form>
+    </div>
+		
+    <?php if ( $site_slogan): ?>
+          <p class="header-site-slogan"><?php print $site_slogan; ?></p>
+        <?php endif; ?>
 
-    <?php if ($navigation or $menu): ?>
-      <?php if ($menu) print $menu; ?>
-      <?php if ($navigation) print $navigation; ?>
-    <?php endif; ?>
+    <nav class="su-main-nav no-js su-main-nav--mobile-search" aria-label="main menu">
+      <button class="su-main-nav__toggle su-main-nav__toggle--right" aria-expanded="false">Menu</button>
+
+      <?php if ($navigation or $menu): ?>
+          <?php if ($menu) print $menu; ?>
+          <?php if ($navigation) print $navigation; ?>
+      <?php endif; ?>
+
+      <div class="su-site-search" role="search">
+        <form action="/search" method="get" accept-charset="UTF-8">
+          <div class="form-type-search form-item-keys">
+            <label for="edit-keys" class="su-site-search__sr-label">Enter your keywords </label>
+            <input class="form-search form-control form-search su-site-search__input" type="search" id="edit-keys" name="keys" value="" size="40" maxlength="255">
+          </div>
+          <div class="form-actions" id="edit-actions"><input class="btn-primary btn form-submit su-site-search__submit su-sr-only-text" type="submit" id="edit-submit" name="op" value=""></div>
+        </form>
+      </div>
+    </nav>
+
   </section>
 </section>
+
