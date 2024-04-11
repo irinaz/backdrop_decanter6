83645afe502dd2a7b70605abb370b93067b6dd50
Boris Ayupov <boris.ayupov@gmail.com>
2024-04-11 11:12:26 -0400

    Correct mistake

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -19,27 +19,6 @@
  * - $menu: The menu for the header (if any), as an HTML string.
  */
 ?>
-<?php
-/**
- * @file
- * Display generic site information such as logo, site name, etc.
- *
- * Available variables:
- *
- * - $base_path: The base URL path of the Backdrop installation. At the very
- *   least, this will always default to /.
- * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
- * - $is_front: TRUE if the current page is the front page.
- * - $logged_in: TRUE if the user is registered and signed in.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $front_page: The URL of the front page. Use this instead of $base_path, when
- *   linking to the front page. This includes the language domain or prefix.
- * - $site_name: The name of the site, empty when display has been disabled.
- * - $site_slogan: The site slogan, empty when display has been disabled.
- * - $menu: The menu for the header (if any), as an HTML string.
- */
-?>
 <a href="#main-content" class="su-skiplinks ">Skip to main content</a>
 <div class="su-brand-bar ">
   <div class="su-brand-bar__container">
